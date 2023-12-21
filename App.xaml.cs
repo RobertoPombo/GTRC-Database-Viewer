ec19d54d448861d1d8d9f9afad71a97fd0956d0c
RobertoPombo <captn.sponge2@web.de>
2023-12-22 00:59:36 +0100

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GTRC_Database_Viewer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
