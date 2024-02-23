5ca68e278bb566b4785f0ac28aacfc523cc982dd
RobertoPombo <captn.sponge2@web.de>
2024-02-23 16:31:49 +0100

    Added Car.NameLfm, Track.NameLfm.

--- Migrations/V0/Car.cs
+++ Migrations/V0/Car.cs
@@ -23,5 +23,25 @@
         }
 
         public string NameGtrc { set { NameGoogleSheets = value; } }
+
+        public new string NameLfm
+        {
+            get
+            {
+                if (AccCarId == 31) { return "Audi R8 LMS GT3 evo II"; }
+                else if (AccCarId == 8) { return "Bentley Continental"; }
+                else if (AccCarId == 20) { return "AMR V8 Vantage"; }
+                else if (AccCarId == 30) { return "BMW M4 GT3"; }
+                else if (AccCarId == 32) { return "Ferrari 296 GT3"; }
+                else if (AccCarId == 21) { return "Honda NSX GT3 Evo"; }
+                else if (AccCarId == 33) { return "Lamborghini Huracan GT3 EVO 2"; }
+                else if (AccCarId == 35) { return "McLaren 720S GT3 Evo"; }
+                else if (AccCarId == 25) { return "Mercedes-AMG GT3"; }
+                else if (AccCarId == 34) { return "Porsche 992 GT3 R"; }
+                else if (AccCarId == 6) { return "Nissan GT-R Nismo GT3"; }
+                else if (AccCarId == 24) { return "Ferrari 488 GT3 Evo"; }
+                else { return string.Empty; }
+            }
+        }
     }
 }
--- Migrations/V0/Track.cs
+++ Migrations/V0/Track.cs
@@ -5,5 +5,37 @@
         public int AccTimePenDT { set { AccTimePenDtS = (ushort)value; } }
 
         public string NameGtrc { set { NameGoogleSheets = value; } }
+
+        public new string NameLfm
+        {
+            get
+            {
+                if (AccTrackId == "imola") { return "Autodromo Enzo e Dino Ferrari"; }
+                else if (AccTrackId == "monza") { return "Autodromo Nazionale di Monza"; }
+                else if (AccTrackId == "brands_hatch") { return "Brands Hatch Circuit"; }
+                else if (AccTrackId == "barcelona") { return "Circuit de Catalunya"; }
+                else if (AccTrackId == "paul_ricard") { return "Circuit de Paul Ricard"; }
+                else if (AccTrackId == "spa") { return "Circuit de Spa Francorchamps"; }
+                else if (AccTrackId == "cota") { return "Circuit Of The Americas"; }
+                else if (AccTrackId == "valencia") { return "Circuit Ricardo Tormo"; }
+                else if (AccTrackId == "donington") { return "Donington Park"; }
+                else if (AccTrackId == "hungaroring") { return "Hungaroring"; }
+                else if (AccTrackId == "indianapolis") { return "Indianapolis"; }
+                else if (AccTrackId == "kyalami") { return "Kyalami"; }
+                else if (AccTrackId == "laguna_seca") { return "Laguna Seca"; }
+                else if (AccTrackId == "misano") { return "Misano"; }
+                else if (AccTrackId == "mount_panorama") { return "Mount Panorama Circuit"; }
+                else if (AccTrackId == "nurburgring") { return "Nürburgring"; }
+                else if (AccTrackId == "oulton_park") { return "Oulton Park"; }
+                else if (AccTrackId == "silverstone") { return "Silverstone"; }
+                else if (AccTrackId == "snetterton") { return "Snetterton"; }
+                else if (AccTrackId == "red_bull_ring") { return "Spielberg - Red Bull Ring"; }
+                else if (AccTrackId == "suzuka") { return "Suzuka Circuit"; }
+                else if (AccTrackId == "watkins_glen") { return "Watkins Glen"; }
+                else if (AccTrackId == "zandvoort") { return "Zandvoort"; }
+                else if (AccTrackId == "zolder") { return "Zolder"; }
+                else { return string.Empty; }
+            }
+        }
     }
 }
