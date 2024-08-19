bfe1e577a67309c1d1bc21f8ea6811fde3161896
RobertoPombo <captn.sponge2@web.de>
2024-08-19 19:06:54 +0200

    Removed Table SeasonCarclass. Added MigrationsV0.EventCarclass.

---
+++ Migrations/V0/EventCarclass.cs
@@ -0,0 +1,7 @@
+﻿namespace GTRC_Database_Viewer.Migrations.V0
+{
+    public class EventCarclass : GTRC_Basics.Models.EventCarclass
+    {
+        public int id { set { Id = value; EventId = value; CarclassId = 3; MaxGridSlots = byte.MaxValue; } }
+    }
+}
--- Migrations/V0/SeasonCarclass.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GTRC_Database_Viewer.Migrations.V0
-{
-    public class SeasonCarclass : GTRC_Basics.Models.SeasonCarclass
-    {
-        public int id { set { Id = value; SeasonId = value; CarclassId = 3; MaxGridSlots = byte.MaxValue; } }
-    }
-}
