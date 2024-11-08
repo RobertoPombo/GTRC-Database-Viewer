5b4cc62086f21affe5fae0ea4c232a9a2edd97e6
RobertoPombo <captn.sponge2@web.de>
2024-11-08 20:39:53 +0100

    Added Migrations.V1.Entry, .EntryDatetime.

---
+++ Migrations/V1/Entry.cs
@@ -0,0 +1,26 @@
+﻿using GTRC_Basics;
+
+namespace GTRC_Database_Viewer.Migrations.V1
+{
+    public class Entry : GTRC_Basics.Models.Entry
+    {
+        public static List<Entry> List = [];
+
+        private int id = GlobalValues.NoId;
+
+        public new int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                bool isInList = false;
+                foreach (Entry obj in List)
+                {
+                    if (obj.Id == value) { isInList = true; break; }
+                }
+                if (!isInList) { List.Add(this); }
+            }
+        }
+    }
+}
---
+++ Migrations/V1/EntryDatetime.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace GTRC_Database_Viewer.Migrations.V1
+{
+    public class EntryDatetime : GTRC_Basics.Models.EntryDatetime
+    {
+        public new bool IsPermanent
+        {
+            get
+            {
+                foreach (Entry entry in V1.Entry.List)
+                {
+                    if (entry.Id == EntryId) { return entry.IsPermanent; }
+                }
+                return new EntryDatetime().IsPermanent;
+            }
+            set { }
+        }
+    }
+}
